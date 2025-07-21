7945bff5eecb7bc6109c66bceb6953d14b21eab2
PRAJU <prajwalpyadav26@gmail.com>
2025-07-21 22:15:21 +0530

    object creation

---
+++ CLASS&OBJECT/Bill.java
@@ -0,0 +1,11 @@
+class Bill{
+String planType;
+double billAmountWithTax;
+String billIssueDate;
+String paymentMethod;
+String billDueDate;
+int billId;
+boolean isTaxInclusive;
+boolean isBillPaid;
+boolean isBillOverDue;
+}
--- METHOD OVERLOADING/InstagramRunner.java
+++
@@ -1,8 +0,0 @@
-class InstagramRunner{
-
-public static void main(String insta[]){
-
-boolean loginUserWithGmail = Instagram.logIn("praju@gmail.com","123abc");
-boolean loginUserWithNum = Instagram.logIn(9645566258L,"145541");
-}
-}
